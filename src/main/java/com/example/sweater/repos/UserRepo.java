164b81a5bafdc16ee64320c738b45c70a8dd72d6
vasekva <vahonin_vasek@mail.ru>
2020-06-28 18:12:56 +0400

    Changed tamplate engine to Freemarker

--- src/main/java/com/example/sweater/repos/UserRepo.java
+++ src/main/java/com/example/sweater/repos/UserRepo.java
@@ -4,5 +4,5 @@ import com.example.sweater.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    User findByUsername(String name);
 }
